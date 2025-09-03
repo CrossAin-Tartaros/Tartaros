d3f8722d320b959cad6a3a114c974b0b8df64a9c
lhr4426 <lhr4426@naver.com>
2025-09-03 21:02:28 +0900

    [feat] 패링 애니메이션 추가

---
+++ Tartaros/Assets/Scripts/ParryAnimation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParryAnimation : MonoBehaviour
+{
+    public void AnimationDone()
+    {
+        Destroy(gameObject);
+    }
+}
