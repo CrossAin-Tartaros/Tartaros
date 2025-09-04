9dfe80759ea5d5aedb34b4e442f22823d770937b
lhr4426 <lhr4426@naver.com>
2025-09-04 20:52:50 +0900

    [feat] Shield Effect

--- Tartaros/Assets/Scripts/Item/Slot/Shield.cs
+++ Tartaros/Assets/Scripts/Item/Slot/Shield.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,21 @@ using UnityEngine;
 public class Shield : MonoBehaviour
 {
     [SerializeField] GameObject shieldEffect;
+    [SerializeField] GameObject useShieldEffect;
     public bool IsShieldOn { get; private set; }
     [SerializeField] int initialShield;
     private int remainShield;
+    private Player player;
 
+    public void Init(Player player)
+    {
+        this.player = player;
+    }
+    
+    private void Start()
+    {
+        GetShield();
+    }
 
     public void GetShield()
     {
@@ -20,17 +32,19 @@ public class Shield : MonoBehaviour
 
     public void UseShield()
     {
+        Debug.Log("Use Shield");
         remainShield--;
-        UIManager.Instance.GetUI<UIShield>().SetShield(remainShield);
-
+        // UIManager.Instance.GetUI<UIShield>().SetShield(remainShield);
+        GameObject go = Instantiate(useShieldEffect, player.GetAimPoint(0.8f),  Quaternion.identity);
+        var psRenderer = go.GetComponent<ParticleSystemRenderer>();
+        psRenderer.sortingOrder = 200;
+        go.SetActive(true);
+        
         if (remainShield == 0)
         {
             IsShieldOn = false;
             shieldEffect.SetActive(false);
-            UIManager.Instance.GetUI<UIShield>().CloseUI();
+            // UIManager.Instance.GetUI<UIShield>().CloseUI();
         }
     }
-    
-
-
 }
