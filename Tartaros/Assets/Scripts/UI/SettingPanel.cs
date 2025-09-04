0b103f417161a4b28f79498dc860fdd81147e96e
wong1125 <wong1125@naver.com>
2025-09-04 23:04:59 +0900

    [Fix] 세팅패널 버그 수정

--- Tartaros/Assets/Scripts/UI/SettingPanel.cs
+++ Tartaros/Assets/Scripts/UI/SettingPanel.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class SettingPanel : UIBase
 {
-    
+    private void OnEnable()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1f;
+    }
 }
