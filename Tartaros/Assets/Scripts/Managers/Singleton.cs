250a7b22c9950019218a8f0a85b7333a282e50bb
wong1125 <wong1125@naver.com>
2025-09-05 10:37:10 +0900

    [Fix] 오류로 버그 수정 롤백

--- Tartaros/Assets/Scripts/Managers/Singleton.cs
+++ Tartaros/Assets/Scripts/Managers/Singleton.cs
@@ -5,22 +5,11 @@ using UnityEngine;
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     protected static T _instance;
-
-    private static bool isShuttingDown = false; // 종료 플래그
-
-    public static T Instance
-    {
+    	public static T Instance
+    	{
     		get
     		{
-            // 애플리케이션 종료 시 인스턴스 재생성 방지
-            if (isShuttingDown)
-            {
-                Debug.LogWarning($"[Singleton] Instance '{typeof(T)}' is already destroyed on application quit. Won't create again - returning null.");
-                return null;
-            }
-
-
-            if (_instance == null)
+    			if(_instance == null)
     			{
     				_instance = FindObjectOfType<T>();
     				if(_instance == null)
@@ -36,21 +25,9 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     			}
     			return _instance;
     		}
-    }
-    public static bool IsCreatedInstance()
-    {
+    	}
+    	public static bool IsCreatedInstance()
+    	{
     		return (_instance != null);
-    }
-
-    // 종료 시 플래그 설정
-    protected virtual void OnApplicationQuit()
-    {
-        isShuttingDown = true;
-    }
-
-    // 오브젝트 파괴 시에도 플래그 설정 (씬 전환 등)
-    protected virtual void OnDestroy()
-    {
-        isShuttingDown = true;
-    }
+    	}
 }
